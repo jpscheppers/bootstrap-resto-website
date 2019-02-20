d4da9b7ddf72908f930a0d84b924da51c0bc51f4
jpscheppers <jpscheppers@gmail.com>
2019-02-20 23:02:58 +0100

    dailies

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,8 @@
 /* var name = prompt("Cher visiteur, quel est votre prénom?");
 document.getElementById("name").textContent= name; */
+
+
+// MDB Lightbox Init
+$(function () {
+$("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
+});
